c7f5773080bf0de9bc02cd68b4cc2dca0a020767
David <ummmmmpie@gmail.com>
2019-03-24 01:42:20 -0400

    3/23/2019

--- ur_dnguyen72.py
+++ ur_dnguyen72.py
@@ -102,6 +102,8 @@ if __name__ == '__main__':
     if args.list:
         print(args.list + ' list for ' + args.filename[0])
         print(len(str(args.list) + ' list for ' + args.filename[0]) * '=')
+        print(read_login_rec())
+        exit()
         print(*sorted(read_login_rec(unformatted_login_recs)), sep="\n")
 
     elif args.type:
