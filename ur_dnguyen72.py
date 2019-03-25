5e31ecf4b06c2a2891917d0756d4b7884d1da82c
David <ummmmmpie@gmail.com>
2019-03-24 21:06:44 -0400

    3/23/2019

--- ur_dnguyen72.py
+++ ur_dnguyen72.py
@@ -107,9 +107,6 @@ if __name__ == '__main__':
         print(*sorted(unformatted_login_recs), sep="\n")
 
     if args.type:
-        print(args.type.title())
-        print(args.user or args.rhost)
-        exit()
         print(args.type.title() + ' usage report for ' + str(args.user or args.rhost))
         print(len(args.type + ' usage report for ' + str(args.user or args.rhost) * '='))
         if args.type == 'daily':
