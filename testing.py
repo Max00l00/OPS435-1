abbeeb079e6675c1c080984f6b6cc15f9ee4785c
David <ummmmmpie@gmail.com>
2019-03-24 20:42:01 -0400

    3/23/2019

--- testing.py
+++ testing.py
@@ -1,31 +1,77 @@
 #!/usr/bin/env python3
 
-import os
-
-def get_login_rec():
+def read_login_rec(filelist):
     """
-    The get_login_rec function will run when no file name is given but instead the
-    string "last". The function will return a unformatted list from the filtered records
-    of the the output from the last command.
+    The read_login_rec takes one argument being a given log file name. The function will
+    an unformatted list from the data from the the given file.
     """
 
-    last = 'last -Fiw'
-    x = os.popen(last)
-    results = x.readlines()
+    x = open(filelist, 'r')
+    login_recs = x.readlines()
     x.close()
-    login_recs = []
-    for item in results:
-        if len(item.split()) == 15:
-            login_recs.append(item)
-    return login_recs
+    filtered_recs = []
+    if args.list:
+        if args.list == 'user':
+            for item in login_recs:
+                filtered_recs.append(item.split()[0])  # Grab only user names
+        if args.list == 'host':
+            for item in login_recs:
+                filtered_recs.append(item.split()[2])  # Grab only host IPs
+        return set(filtered_recs)
+    else:
+        return set(login_recs)
 
-def get_usernames(login_rec):
-
-    username_list = []
-    for item in login_rec:
-        username_list.append(item.split()[0])
-    return username_list
 
 if __name__ == '__main__':
-    print(*sorted(get_usernames(get_login_rec())),sep="\n")
-    print()
+
+    # Arguments
+
+    parser = argparse.ArgumentParser(description='Usage Report based on the last command',
+                                     epilog='\nCopyright 2018 - David Nguyen')
+    parser.add_argument('filename', metavar='F', nargs='*', default='empty', help='list of files to be processed')
+    parser.add_argument('-l', '--list', choices=['user', 'host'],
+                        help='generates user name or remote host IP from the given files')
+    parser.add_argument('-r', '--rhost', metavar='RHOST', help='usage report for the given remote host IP')
+    parser.add_argument('-t', '--type', choices=['daily', 'weekly', 'monthly'],
+                        help='type of report: daily, weekly, and monthly')
+    parser.add_argument('-u', '--user', help='usage report for the given user name')
+    parser.add_argument('-v', '--verbose', help='tune on unformatted_login_recs verbosity', action='store_true')
+    args = parser.parse_args()
+
+    unformatted_login_recs = []
+
+    if "last" in args.filename:
+        unformatted_login_recs.extend(get_login_rec())
+    else:
+        for item in args.filename:
+            unformatted_login_recs.extend(read_login_rec(args.filename[0]))
+
+    if args.verbose:
+        print('Files to be processed: ' + str(args.filename))
+        print('Type of args for files ' + str(type(args.filename)))
+
+        if args.list:
+            print('processing usage report for the following:')
+            print('reading login/logout record files ' + str(args.filename))
+            print('Generating list for ' + str(args.list))
+        else:
+            if args.rhost:
+                print('usage report for remote host: ' + str(args.rhost))
+            else:
+                print('usage report for user: ' + str(args.user))
+            print('usage report type:', str(args.type))
+            print('processing usage report for the following:')
+            print('reading login/logout record files', str(args.filename))
+
+    if args.type:
+        print(args.type + ' usage report for ' + args.user or args.rhost)
+        print(len(args.type + ' usage report for ' + args.user or args.rhost) * '=')
+        if args.type == 'daily':
+            print('{:13} {:>13}'.format('Date', 'Usage in seconds'))
+        #            print(cal_daily_usage(args.rhost or args.user, unformatted_login_recs))
+        elif args.type == 'weekly':
+            print('{:12} {:>12}'.format('Week #', 'Usage in seconds'))
+        #            print(cal_monthly_usage(args.rhost or args.user, unformatted_login_recs))
+        elif args.type == 'monthly':
+            print('{:11} {:>11}'.format('Month', 'Usage in seconds'))
+        #           print(cal_monthly_usage(args.rhost or args.user, unformatted_login_recs))
